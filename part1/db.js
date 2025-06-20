7fa812a5e27426bcafeb55717a19a110b9310a69
Ellie2-boop <a1902978@adelaide.edu.au>
2025-06-20 01:02:53 +0000

    [automatic commit] 2025-06-20 01:02:53 UTC

--- part1/db.js
+++ part1/db.js
@@ -2,11 +2,13 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: 'localhost',
+  user: 'root',
+  password: 'Yes',
+  database: 'dogwalkssql',
   multipleStatements: true
 });
+
 
 module.exports = pool;
 
