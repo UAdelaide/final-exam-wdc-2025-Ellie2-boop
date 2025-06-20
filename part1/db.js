50424f3207fc1ee3fd9b3982c0346abdddc440f1
Ellie2-boop <a1902978@adelaide.edu.au>
2025-06-20 03:05:48 +0000

    [automatic commit] 2025-06-20 03:05:48 UTC

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Yes',
+  password: 'YourStrongPassword',
   database: 'DogWalkService',
   multipleStatements: true
 });
