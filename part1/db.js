eef3c3c7825a1866c2c5862d61ca4b1aabdfc684
Ellie2-boop <a1902978@adelaide.edu.au>
2025-06-20 00:42:45 +0000

    [automatic commit] 2025-06-20 00:42:45 UTC

--- part1/db.js
+++ part1/db.js
@@ -19,12 +19,3 @@ console.log('DB settings:', {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  multipleStatements: true
-});
-
-module.exports = pool;
