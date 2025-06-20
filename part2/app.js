777e26b74175f4aaf8f75e5c214db0d3d2475996
Ellie2-boop <a1902978@adelaide.edu.au>
2025-06-20 07:58:35 +0000

    [automatic commit] 2025-06-20 07:58:35 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const pool = require('./models/db');  // Adjust path if needed
+const pool = require('./models/db');// Adjust path if needed
 
 
 const app = express();
