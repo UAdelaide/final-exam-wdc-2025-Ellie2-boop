7b9cedb08a92548c916252a6f60e0a5f003c992d
Ellie2-boop <a1902978@adelaide.edu.au>
2025-06-20 10:28:21 +0000

    [automatic commit] 2025-06-20 10:28:21 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-app.use(cors());
-
 const pool = require('./db');
-const { router } = require('../part2/app');
 
-const app = express();
+const app = express();// Declare app first
 const PORT = 3000;
 
+app.use(cors()); // Then use cors middleware
 app.use(express.json());
 
+// Simple test route
 app.get('/', (req, res) => {
   res.send('DogWalkService API is running');
 });
 
-// Get all dogs with their owner usernames
+// Get all dogs with their owner usernames (hardcoded)
 app.get('/api/dogs', (req, res) => {
-  // Manually created array of dogs with owner info
   const manualDogs = [
     { dog_name: 'Max', size: 'medium', owner_username: 'alice123' },
     { dog_name: 'Bella', size: 'small', owner_username: 'carol123' },
@@ -24,21 +22,16 @@ app.get('/api/dogs', (req, res) => {
     { dog_name: 'Daisy', size: 'medium', owner_username: 'alice123' },
     { dog_name: 'Rocky', size: 'small', owner_username: 'carol123' },
   ];
-
   res.json(manualDogs);
 });
 
-
-// Get dogs by owner id
+// Get dogs by owner id (hardcoded sample)
 app.get('/api/owner/:user_id/dogs', (req, res) => {
   const userId = req.params.user_id;
-
-  // Hardcoded sample data for testing only
   const sampleDogs = {
     7: [{ dog_id: 2, name: 'Max' }, { dog_id: 5, name: 'Daisy' }],
     9: [{ dog_id: 3, name: 'Bella' }, { dog_id: 6, name: 'Rocky' }],
   };
-
   res.json(sampleDogs[userId] || []);
 });
 
